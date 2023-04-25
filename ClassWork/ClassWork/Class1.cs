d2a3ecf1bb3ef875c373b88d37e0f9e8e25d8565
Monique Jackson <104055015+ST10096757@users.noreply.github.com>
2023-04-25 08:34:21 +0200

    Add files via upload

---
+++ ClassWork/ClassWork/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork
+{
+    internal class Class1
+    {
+        private void ArrayMethod()
+        {
+            var myArray = new int[5];
+
+            int[] myArray2 = new[] { 1, 2, 3, 4, 5};
+
+            Array.Clear(myArray, 0, myArray.Length);
+            //I blacked out. I have no idea what this does.
+
+            //Capital Array is a class
+            //the other is a value/reference type thingy
+            //I'm killing it with these notes.
+
+            var myTypeArray = new StudentClass[5];//this references code that I have not typed yet
+            //this "<>" is generic
+            //...which means something
+            //that I forgot
+            //But I think it could potentially be important.
+
+            var st.StudentClass = myTypeArray[0].StudentClass;
+            //can access objects in like, a class thing.
+            //like accessing "student name" from the class "student class"
+
+            foreach(StudentClass studentObject in myTypeArray)
+            {
+                studentObject.StudentName;
+            }
+
+            //we declared a student class
+            //then we make objects
+            //then we make an array to store the class, then that make the array 
+            //- populate it with the objects
+        }
+    }
+}
---
+++ ClassWork/ClassWork/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //We're trying to do class work because I literally have no fucking clue what's 
+            //happening in C#
+
+            //arrays need to be declared as a CONSTANT because reasons...
+            //array will only use a set size |(and then doesn't lus to change 
+            //because fuck you)
+
+            //to lengthen array, use the "NEW" key word
+            //but then apparently the data of the old array is gone...?
+
+            //all functions in ArrayLists are in Arrays (class), they just gatekept it
+            //Uncle Rodolph avoids double arrays...I think that's what he said
+
+            //use "new" to make two copies of the same kak in memory (with objects)
+            //we're testing to see if it also works with array
+
+
+
+            //you can use "var" in arrays too
+        }
+    }
+}
